bdb96eecfec4fcdb027e4807bc1c223c94b22896
Ife Njock-Barnes <ife1998@hotmail.com>
2023-05-02 21:56:11 +0100

    removed extra file

--- src/images.js
+++
@@ -1,9 +0,0 @@
-const images = [
-  { src: "dog 1.jpg", alt: "Image 1" },
-  { src: "dog 2.jpg", alt: "Image 2" },
-  { src: "dog 3.jpg", alt: "Image 3" },
-  { src: "dog 4.jpg", alt: "Image 4" },
-  { src: "dog 5.jpg", alt: "Image 5" },
-];
-
-<ImageCarousel images={images} />;
