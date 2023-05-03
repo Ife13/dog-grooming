b191d58c353b8de9e8826a9fe7d5df575ff63cc0
Ife Njock-Barnes <ife1998@hotmail.com>
2023-05-03 17:10:55 +0100

    carousel code

--- src/image-carousel.jsx
+++ src/image-carousel.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import dog1 from "../src/images/dog 1.jpg";
+import dog2 from "../src/images/dog 2.jpg";
+import dog3 from "../src/images/dog 3.jpg";
+import dog4 from "../src/images/dog 4.jpg";
+import dog5 from "../src/images/dog 5.jpg";
 
-function carousel() {
-  return (
-    <div>
-      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
-      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
-      {images.map((image, index) => {
-        return (
-          <div
-            key={index}
-            className={index === currentImage ? "slide active" : "slide"}
-          >
-            {index === currentImage && (
-              <img src={image.src} alt={image.alt} className="image" />
-            )}
-          </div>
-        );
-      })}
-    </div>
-  );
-}
+const handleDragStart = (e) => e.preventDefault();
+
+const items = [
+  <img src={dog1} onDragStart={handleDragStart} role="presentation" />,
+  <img src={dog2} onDragStart={handleDragStart} role="presentation" />,
+  <img src={dog3} onDragStart={handleDragStart} role="presentation" />,
+  <img src={dog4} onDragStart={handleDragStart} role="presentation" />,
+  <img src={dog5} onDragStart={handleDragStart} role="presentation" />,
+];
+
+const carousel = () => {
+  return <AliceCarousel mouseTracking items={items} />;
+};
 
 export default carousel;
